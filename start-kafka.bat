4bd28e1e12ce14348b53e09b7550a6cec663f125
Abu Raihan <xbura1han@gmail.com>
2022-10-27 10:18:26 +0600

    Add: clear cache
    Fix: Kafka is not starting sometimes on windows

--- start-kafka.bat
+++ start-kafka.bat
@@ -2,5 +2,9 @@
 
 %cd%
 
+# Clear cache
+rmdir /s /q "C:\tmp\kafka-logs"
+rmdir /s /q "C:\tmp\zookeeper"
+
 start "" .\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties
 start "" .\bin\windows\kafka-server-start.bat .\config\server.properties
